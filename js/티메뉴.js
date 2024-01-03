a19462aa2e6aea2cc79fd8247391a6d92886bdc2
haeun9634 <103066456+haeun9634@users.noreply.github.com>
2024-01-03 12:56:06 +0900

    Update 티메뉴.js

--- js/티메뉴.js
+++ js/티메뉴.js
@@ -1,6 +1,7 @@
 
-document.write('<script src="../js/시그니처.js"> </script>');
-var scripts = document.querySelectorAll('script[src="../js/시그니처.js"]');
+
+document.write('<script src="../js/메뉴데이터저장.js"> </script>');
+var scripts = document.querySelectorAll('script[src="../js/메뉴데이터저장.js"]');
 if (scripts.length > 1) {
     scripts[1].parentNode.removeChild(scripts[1]);
 }
@@ -10,27 +11,27 @@ function TeaClick(idd){
     let now=document.querySelector(`#${idd}`);
     switch(idd){
         case "Chamomile":
-            cham++,n++;//각개수, 총개수 늘리기
+            cham++;
             printChamomile();
             break;
         case "GingerLemon":
-            gl++,n++;//각개수, 총개수 늘리기
+            gl++;
             printGingerLemon();
             break;
         case "GrapeFruit":
-            gf++; n++;
+            gf++;
             printGrapeFruit();
             break;
         case "PepperMint":
-            pm++; n++;
+            pm++;
             printPepperMint();
             break;
         case "BlackMilkTea":
-            bmt++; n++;
+            bmt++;
             printBlackMilkTea();
             break;
         case "EarlGray":
-            eg++; n++;
+            eg++;
             printEarlGray();
             break;
     }
@@ -38,162 +39,6 @@ function TeaClick(idd){
     totalnum();
     savemenu();
 }
-
-// function makePrice(){//가격 정하기
-//     Price2=4000*Number(cham)+4500*Number(gl)+5000*Number(gf)
-//     +4000*Number(pm)+4500*Number(bmt)+4000*Number(eg);
-//     Price.innerHTML=`${Price2}원`;
-// }
-
-// function totalnum(){
-//     let tn=document.querySelector(".totalnum");
-//     tn.innerHTML=`<br>총개수 <b>${n}</b>개`;
-//     tn.style.top='180px';
-//        //totalnum을 마지막 요소의 아래로 이동
-//     if (row >= 5) {
-//         let blankElements = document.querySelectorAll(".blank");
-//         let pmstblank = blankElements[blankElements.length - 1];
-//         tn.style.top = pmstblank.offsetTop + pmstblank.offsetHeight + 'px';; // 10은 여백
-//     }
-// }
-
-// let now=5;
-// function blank(neww){
-//     if(now<neww){//이전값보다 크다면 blank삭제
-//         now=neww;
-//         let blank=document.querySelector(".blank");
-//         selectmenu.removeChild(blank);
-//     }
-//     let text=document.createElement("div");
-//     text.innerHTML=`<br>`
-//     text.setAttribute("class","blank");
-//     selectmenu.appendChild(text);
-// }
-
-// function SelectClick(classs,num){
-//     let nowcalgf=document.querySelector(`.${classs}`);
-//     switch(num){
-//         case "cham":
-//             cham--;
-//             break;
-//         case "gl":
-//             gl--;
-//             break;
-//         case "gf":
-//             gf--;
-//             break;
-//         case "pm":
-//             pm--;
-//             break;
-//         case "bmt":
-//             bmt--;
-//             break;
-//         case "eg":
-//             eg--;
-//             break;
-//     }
-
-//     n--;//총개수 삭제
-
-//     if(cham<=0 && chamrow!=0) rowremove("Chamomile","cham",chamrow);
-//     if(gl<=0 && glrow!=0) rowremove("GingerLemon","gl",glrow);
-//     if(gf<=0 && gfrow!=0) rowremove("GrapeFruit","gf",gfrow);
-//     if(pm<=0 && pmrow!=0) rowremove("PepperMint","pm",pmrow);
-//     if(bmt<=0 && bmtrow!=0) rowremove("BlackMilkTea","bmt",bmtrow);
-//     if(eg<=0 && egrow!=0) rowremove("EarlGray","eg",egrow);
-
-//     totalprint();
-// }
-
-// function rowremove(classs,num,rownum){
-//     let nowclass=document.querySelector(`.${classs}`);
-//     switch(num){
-//         case "cham":
-//             n-=Number(cham);
-//             cham=0; chamrow=0; 
-//             break;
-//         case "gl":
-//             n-=Number(gl);
-//             gl=0; glrow=0; 
-//             break;
-//         case "gf":
-//             n-=Number(gf);
-//             gf=0; gfrow=0; 
-//             break;
-//         case "pm":
-//             n-=Number(pm);
-//             pm=0; pmrow=0; 
-//             break;
-//         case "bmt":
-//             n-=Number(bmt);
-//             bmt=0; bmtrow=0; 
-//             break;
-//         case "eg":
-//             n-=Number(eg);
-//             eg=0; egrow=0;
-//             break;
-//     }
-
-//      /*현재 열보다 숫자가 더 크면 하나씩 줄이기*/
-//     if(chamrow>rownum) chamrow--;
-//     if(glrow>rownum) glrow--;
-//     if(gfrow>rownum) gfrow--;
-//     if(pmrow>rownum) pmrow--;
-//     if(bmtrow>rownum) bmtrow--;
-//     if(egrow>rownum) egrow--;
-
-//     if(row==5){/*row가 5일경우 blank삭제*/
-//         let blank=document.querySelector(".blank");
-//         selectmenu.removeChild(blank);
-//         now=5
-//     }
-    
-//     row--;//전체 열 하나 줄이기
-//     selectmenu.removeChild(nowclass);
-
-//     totalprint();
-//     /*cancleBubble을 이용해 이벤트 버블링 방지*/
-//     if (window.event) {
-//         window.event.cancelBubble = true;
-//       } else {
-//         event.stopPropagation();
-//       }
-
-// }
-
-// function totalprint(){
-//     printChamomile();
-//     printGingerLemon();
-//     printGrapeFruit();
-//     printPepperMint();
-//     printBlackMilkTea();
-//     printEarlGray();
-
-//     makePrice();
-//     totalnum();
-
-// }
-
-// function totalcancel(){
-//     if(cham!=0) selectmenu.removeChild(document.querySelector(".Chamomile"));
-//     if(gl!=0) selectmenu.removeChild(document.querySelector(".GingerLemon"));
-//     if(gf!=0) selectmenu.removeChild(document.querySelector(".GrapeFruit"));
-//     if(pm!=0) selectmenu.removeChild(document.querySelector(".PepperMint"));
-//     if(bmt!=0) selectmenu.removeChild(document.querySelector(".BlackMilkTea"));
-//     if(eg!=0) selectmenu.removeChild(document.querySelector(".EarlGray"));
-//     if(row>=5){
-//         let blank=document.querySelector(".blank");
-//         selectmenu.removeChild(blank);
-//     }
-
-//     cham=0,gl=0,gf=0,pm=0,bmt=0,eg=0;//각 개수
-//     n=0, row=0; //총개수, 행개수
-//     chamrow=0,glrow=0,gfrow=0,pmrow=0,bmtrow=0,egrow=0; //각 열번호
-    
-
-//     makePrice();
-//     totalnum();
-// }
 
 
 
